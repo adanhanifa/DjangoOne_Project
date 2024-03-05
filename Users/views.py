3a024763d2e6831bdc20af6a90c3c786f99e078e
adanhanifa <adanhanifa@gmail.com>
2024-03-05 14:34:24 +0300

    first commit

---
+++ Users/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, "index.html")
+def aboutus(request):
+    return render(request, "aboutus.html")
+def contact(request):
+    return render(request, "contact.html")
+def gallery(request):
+    return render(request, "gallery.html")
